b3788b81d69d5dd9139dbfb944e3023570c418ab
Mahreen Qureshi <mahreenq@hotmail.com>
2019-09-12 18:33:28 -0400

    removing comments from app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-// import getMuiTheme from 'material-ui/styles/getMuiTheme';
-// import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 
 import { Provider } from "react-redux";
 import store from "./redux/store";
@@ -10,7 +8,6 @@ import store from "./redux/store";
 import Layout from "./components/Layout";
 import Home from "./components/Home";
 import About from "./components/About";
-//import Projects from "./containers/Projects";
 import Resume from "./components/Resume";
 import NotFound from "./components/NotFound";
 import ProjectList from "./components/ProjectList";
@@ -23,17 +20,15 @@ class App extends Component {
     return (
       <BrowserRouter>
         <Provider store={store}>
-          {/* <MuiThemeProvider> */}
-            <Layout>
-              <Switch>
-                <Route exact path="/" component={Home} />
-                <Route exact path="/about" component={About} />
-                <Route exact path="/showcase" component={ProjectList} />
-                <Route exact path="/resume" component={Resume} />
-                <Route component={NotFound} />
-              </Switch>
-            </Layout>
-          {/* </MuiThemeProvider> */}
+          <Layout>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/about" component={About} />
+              <Route exact path="/showcase" component={ProjectList} />
+              <Route exact path="/resume" component={Resume} />
+              <Route component={NotFound} />
+            </Switch>
+          </Layout>
         </Provider>
       </BrowserRouter>
     );
